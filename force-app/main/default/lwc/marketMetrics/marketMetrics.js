245fcadf3d158fd3c6173d543adcc361740f6ac8
Shawn Soneja <shawn.soneja85@gmail.com>
2023-03-27 16:37:22 -0400

    trying to expose all components...

--- force-app/main/default/lwc/marketMetrics/marketMetrics.js
+++ force-app/main/default/lwc/marketMetrics/marketMetrics.js
@@ -3,7 +3,7 @@ import { LightningElement, wire, track, api } from 'lwc';
 import getFinancialData from '@salesforce/apex/FinancialDataManager.getFinancialData';
 
 import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
-import TRAILHEAD_LOGO from '@salesforce/resourceUrl/chartjs_v280';
+import chartjs from '@salesforce/resourceUrl/chartjs_v280';
 //https://d5e0000012hzdeai-dev-ed.lightning.force.com/resource/1678834348000/chartjs_v280
  
 //table columns for bureau of labor statistics callout
@@ -26,36 +26,40 @@ export default class HelloWorld extends LightningElement {
   columns = columns_bls;
   
   connectedCallback() {
-    //this.callBureauOfLaborStatistics();
-    getFinancialData()
-      .then(result => {
-        this.unemploymentData = result.filter((eachItem) => eachItem.Type__c == 'Unemployment');
-        var dataPoints = [];
-        var timePoints = [];
-        this.unemploymentData.sort((a,b) => (a.Date__c > b.Date__c) ? 1 : ((b.Date__c > a.Date__c) ? -1 : 0));
-        this.unemploymentData.forEach((item) => {
-          dataPoints.push(item.Value__c);
-          timePoints.push(item.Date__c);
-        })
-        this.unemploymentDataPoints = dataPoints;
-        this.unemploymentTimePoints = timePoints;
+    loadScript(this, chartjs + '/chartjs_v280.js').then( () => {
+      //this.callBureauOfLaborStatistics();
+      getFinancialData()
+        .then(result => {
+          this.unemploymentData = result.filter((eachItem) => eachItem.Type__c == 'Unemployment');
+          var dataPoints = [];
+          var timePoints = [];
+          this.unemploymentData.sort((a,b) => (a.Date__c > b.Date__c) ? 1 : ((b.Date__c > a.Date__c) ? -1 : 0));
+          this.unemploymentData.forEach((item) => {
+            dataPoints.push(item.Value__c);
+            timePoints.push(item.Date__c);
+          })
+          this.unemploymentDataPoints = dataPoints;
+          this.unemploymentTimePoints = timePoints;
 
-        this.inflationData = result.filter((eachItem) => eachItem.Type__c == 'CPI');
-        var dataPoints_inflation = [];
-        var timePoints_inflation = [];
-        this.inflationData.sort((a,b) => (a.Date__c > b.Date__c) ? 1 : ((b.Date__c > a.Date__c) ? -1 : 0));
-        this.inflationData.forEach((item) => {
-          dataPoints_inflation.push(item.Value__c);
-          timePoints_inflation.push(item.Date__c);
+          this.inflationData = result.filter((eachItem) => eachItem.Type__c == 'CPI');
+          var dataPoints_inflation = [];
+          var timePoints_inflation = [];
+          this.inflationData.sort((a,b) => (a.Date__c > b.Date__c) ? 1 : ((b.Date__c > a.Date__c) ? -1 : 0));
+          this.inflationData.forEach((item) => {
+            dataPoints_inflation.push(item.Value__c);
+            timePoints_inflation.push(item.Date__c);
+          })
+          this.inflationDataPoints = dataPoints_inflation;
+          this.inflationTimePoints = timePoints_inflation;
+          this.isLoading = false;
         })
-        this.inflationDataPoints = dataPoints_inflation;
-        this.inflationTimePoints = timePoints_inflation;
-        this.isLoading = false;
-      })
-      .catch(error => {
-        console.log('error' + JSON.stringify(error));
-        this.isLoading = false;
-    });
+        .catch(error => {
+          console.log('error' + JSON.stringify(error));
+          this.isLoading = false;
+      });
+    })
+
+    
   }
   renderedCallback(){
     //console.log('rendered');
