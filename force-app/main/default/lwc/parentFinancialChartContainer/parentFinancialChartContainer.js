1ea44f923a1b21b318720533131fbc6dfae6f710
Shawn Soneja <shawn.soneja85@gmail.com>
2023-04-19 22:03:53 -0400

    moving all logic to the parent for ease of starting here; also fixed blank error

--- force-app/main/default/lwc/parentFinancialChartContainer/parentFinancialChartContainer.js
+++ force-app/main/default/lwc/parentFinancialChartContainer/parentFinancialChartContainer.js
@@ -1,13 +1,60 @@
 import { LightningElement, wire, track, api } from 'lwc';
 import getFinancialData from '@salesforce/apex/FinancialDataManager.getFinancialData';
 import { loadScript } from 'lightning/platformResourceLoader';
+import chartjs from '@salesforce/resourceUrl/ChartJs';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 
 
 export default class ParentFinancialChartContainer extends LightningElement {
     @track financialData;
     @track unemploymentData;
-    inflationData;
+    inflationData = [{x: "12/02/2022", y:50}, {x: "12/05/2022", y:60}];
+    @track isChartJsInitialized;
+    chart;
+    @track config = {
+        type: 'line',
+        data: {
+            labels: ["January"],
+            datasets: [{
+                fill: false,
+                label: 'Inflation Rate',
+                data: this.chartData,
+                backgroundColor: [
+                    'rgba(37, 150, 190, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(37, 150, 190, 1)'
+                ],
+                pointBackgroundColor: 'rgba(37, 150, 190, 0.2)',
+                pointBorderColor: 'rgba(37, 150, 190, 1)'
+            },
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'CPI Data'
+            },
+            scales: {
+                xAxes: [{
+                    type: 'time',
+
+                    
+                }],
+                yAxes: [{
+                    type: 'linear',
+                    ticks: {
+                        autoSkip: true,
+                        suggestedMin: 0,
+                        suggestedMax: 1,
+                        stepSize: 0.1,
+                    }
+                }]
+            },
+        }
+    };
+    
     //@track interestRateData;
     //@track gdpData;
 
@@ -30,6 +77,35 @@ export default class ParentFinancialChartContainer extends LightningElement {
                     }
                 })
                 this.inflationData = inflationDataPoints;
+
+                loadScript(this, chartjs + '.js').then(() => {
+                    console.log('script loaded');
+                    this.isChartJsInitialized = true;
+                //org
+                //loadScript(this, chartjs).then(() => {
+                    
+                    this.config.data.datasets[0].data = this.inflationData;
+                    console.log('config: ' + JSON.stringify(this.config.data.datasets[0].data));
+                    const canvas = document.createElement('canvas');
+                    this.template.querySelector('div.chart').appendChild(canvas);
+                    const ctx = canvas.getContext('2d');
+                    this.chart = new window.Chart(ctx, this.config);
+                    this.chart.canvas.parentNode.style.height = '100%';
+                    this.chart.canvas.parentNode.style.width = '100%';
+                }).catch(error => {
+                    console.log("Error:", JSON.stringify(error));
+                    
+                    
+                });
             })
+            
     } 
+
+    
+
+    connectedCallback(){
+        //
+
+        
+    }
 }
