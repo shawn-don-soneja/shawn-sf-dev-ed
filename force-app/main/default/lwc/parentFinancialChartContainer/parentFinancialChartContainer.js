92a999e614d989c222e24ca986d600640e680553
Shawn Soneja <shawn.soneja85@gmail.com>
2023-04-19 22:32:41 -0400

    adding another unique chart is breaking things again

--- force-app/main/default/lwc/parentFinancialChartContainer/parentFinancialChartContainer.js
+++ force-app/main/default/lwc/parentFinancialChartContainer/parentFinancialChartContainer.js
@@ -39,7 +39,9 @@ export default class ParentFinancialChartContainer extends LightningElement {
             scales: {
                 xAxes: [{
                     type: 'time',
-
+                    time:{
+                        min: 1681956535,
+                    }
                     
                 }],
                 yAxes: [{
@@ -67,31 +69,47 @@ export default class ParentFinancialChartContainer extends LightningElement {
                 sortedData.sort((a,b) => (a.Date__c > b.Date__c) ? 1 : ((b.Date__c > a.Date__c) ? -1 : 0));
 
                 //format the data to pass to child charts
-                var unemploymentData = [];
+                var unemploymentDataPoints = [];
                 var inflationDataPoints = [];
                 sortedData.forEach((item) => {
                     if(item.Type__c == 'Unemployment'){
-                        unemploymentData.push({x: item.Date__c, y: item.Value__c});
+                        unemploymentDataPoints.push({x: item.Date__c, y: item.Value__c});
                     }else if(item.Type__c == 'CPI'){
                         inflationDataPoints.push({x: item.Date__c, y: item.Value__c});
                     }
                 })
                 this.inflationData = inflationDataPoints;
+                this.unemploymentData = unemploymentDataPoints;
 
                 loadScript(this, chartjs + '.js').then(() => {
                     console.log('script loaded');
+                    console.log('config: ' + JSON.stringify(this.config.data.datasets[0].data));
                     this.isChartJsInitialized = true;
                 //org
                 //loadScript(this, chartjs).then(() => {
                     
-                    this.config.data.datasets[0].data = this.inflationData;
-                    console.log('config: ' + JSON.stringify(this.config.data.datasets[0].data));
+                    var inflationConfig = {...this.config};
+                    inflationConfig.data.datasets[0].data = this.inflationData;
                     const canvas = document.createElement('canvas');
                     this.template.querySelector('div.chart').appendChild(canvas);
                     const ctx = canvas.getContext('2d');
-                    this.chart = new window.Chart(ctx, this.config);
+                    
+                    this.chart = new window.Chart(ctx, inflationConfig);
                     this.chart.canvas.parentNode.style.height = '100%';
                     this.chart.canvas.parentNode.style.width = '100%';
+
+                    /*
+                    
+                    */
+                    var unemploymentConfig = {...this.config};
+                    unemploymentConfig.data.datasets[0].data = this.inflationData; //changing this makes everything forking crash :(
+                    const canvas2 = document.createElement('canvas');
+                    this.template.querySelector('div.unemploymentchart').appendChild(canvas2);
+                    const ctx2 = canvas2.getContext('2d');
+                    
+                    this.chart2 = new window.Chart(ctx2, unemploymentConfig);
+                    this.chart2.canvas.parentNode.style.height = '100%';
+                    this.chart2.canvas.parentNode.style.width = '100%';
                 }).catch(error => {
                     console.log("Error:", JSON.stringify(error));
                     
